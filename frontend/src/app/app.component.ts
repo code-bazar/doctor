c643859c58cc56f4bf1979e8f7ae7ea9bfa0370a
Bartłomiej Więckowski <bartlomiej.wieckowski@syncron.com>
2019-04-03 15:54:22 +0200

    all *.css file to *.scss, form styles ready

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,7 +4,7 @@ import {User} from "./user/User";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   user = new User('', '','','');
